2c5c8e174598d3df848bf734d8c49a54db1759f7
puruiwang6 <puruiwang6@gmail.com>
2026-08-25 15:33:02 +0000

    test: expose reconciled resize lifecycle

--- PR-demo/mac_bisect_matrix/deploy_x043_reconciled.sh
+++ PR-demo/mac_bisect_matrix/deploy_x043_reconciled.sh
@@ -5,8 +5,8 @@ set -eu
 D=$(cd "$(dirname "$0")" && pwd)
 DIST=${DIST:-/work/dist/reconciled-142c}
 XRDP_DEB=${XRDP_DEB:-xrdp-dev_0.10.80+git20260825151057.f550e9151f70_amd64.deb}
-XORGXRDP_DEB=${XORGXRDP_DEB:-xorgxrdp-dev_1%3a0.10.80+git20260825152758.25a273a17510_amd64.deb}
-IMAGE=${IMAGE:-localhost/xrdp-bisect:reconciled-f550e915-25a273a-u2404-xfce-notrace}
+XORGXRDP_DEB=${XORGXRDP_DEB:-xorgxrdp-dev_1%3a0.10.80+git20260825153242.ae198443d288_amd64.deb}
+IMAGE=${IMAGE:-localhost/xrdp-bisect:reconciled-f550e915-ae19844-u2404-xfce-notrace}
 BUILD="$D/.build-x043-reconciled"
 
 test -f "$DIST/$XRDP_DEB"
